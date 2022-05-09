3dcdbc8b2deaba7df155a78e8d955e7f299b47a5
Manuel Blatt <manuel.blatt@fkie.fraunhofer.de>
2022-05-09 18:04:52 +0200

    queue: refactor, improve CLI and unlock

--- roamerqueue.py
+++ roamerqueue.py
@@ -20,6 +20,32 @@ FORMATER = logging.Formatter("%(asctime)-15s %(message)s")
 
 #https://stackoverflow.com/questions/22235426/python-multiprocessing-worker-queue
 
+
+##### Settings #####
+WORKER_BASE_CONFIG = "config"
+CLONE_PARTIAL_CONFIGS = "clone_configs"
+
+LOCKFILE_NAME = "queue.lock"
+LOCKFILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), LOCKFILE_NAME)
+
+
+##### Shared #####
+def get_current_server_lock_data():
+    #FIXME: Not atomic
+    if os.path.exists(LOCKFILE_PATH):
+        with open(LOCKFILE_PATH, "r") as f:
+            server_data = json.load(f)
+        pid = server_data["pid"]
+        if not psutil.pid_exists(pid):
+            return
+        return server_data
+    
+
+
+
+
+
+##### Server #####
 class IdLoggingStream:
     def __init__(self, id, send_method):
         self.id = id
@@ -78,16 +104,6 @@ def run_task(task, config, logging_handler):
         logger = logging.getLogger(logger_name)
         logger.removeHandler(logging_handler)
 
-
-
-WORKER_BASE_CONFIG = "config"
-CLONE_PARTIAL_CONFIGS = "clone_configs"
-
-
-#FIXME: use global location
-LOCKFILE_NAME = "queue.lock"
-LOCKFILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), LOCKFILE_NAME)
-
 class FakeConfig:
     def __init__(self, input_config):
         for key in input_config.__dict__.keys():
@@ -96,14 +112,17 @@ class FakeConfig:
 
 class Server:
     def __init__(self):
-        self.get_listener_and_lock()
-        self.start_worker()
+        try:
+            self.get_listener_and_lock()
+            self.start_worker()
 
-        #Network: 
-        self.clients = [] 
-        Thread(target=self.handle_messages_from_workers).start()
-        #Thread(target=self.handle_incomming_connections).start()
-        self.handle_incomming_connections() # this will block
+            #Network: 
+            self.clients = [] 
+            Thread(target=self.handle_messages_from_workers).start()
+            #Thread(target=self.handle_incomming_connections).start()
+            self.handle_incomming_connections() # this will block
+        finally:
+            unlock_server()
 
     def get_loaded_worker_configs(self):
         base = importlib.import_module(WORKER_BASE_CONFIG)
@@ -145,9 +164,6 @@ class Server:
             }
             json.dump(server_data, f)
 
-    def unlock(self):
-        os.remove(LOCKFILE_PATH)
-
     def handle_messages(self, connection):
         for message in iter(connection.recv, "CLOSE"):
             self.work_queue.put(message)
@@ -167,16 +183,30 @@ class Server:
             Thread(target=self.handle_messages, args=(connection,)).start()
 
 
-def get_current_server_lock_data():
-    #FIXME: Not atomic
-    if os.path.exists(LOCKFILE_PATH):
-        with open(LOCKFILE_PATH, "r") as f:
-            server_data = json.load(f)
-        pid = server_data["pid"]
-        if not psutil.pid_exists(pid):
-            return
-        return server_data
-    
+def unlock_server():
+    os.remove(LOCKFILE_PATH)
+
+
+def start_server_safe():
+    server_data = get_current_server_lock_data()
+    if not server_data:
+        #Process(target=Server).start()
+        #Thread(target=Server).start()
+        Server() # blocks
+        time.sleep(0.1)
+    else:
+        LOG.warning("Server is already running. Did not start a new server.")
+    server_data = get_current_server_lock_data()
+    if server_data is None:
+        raise RuntimeError("Could not start roamerqueue server")
+    return server_data
+
+
+
+##### Client #####
+
+def connect_to_server(server_data):
+    return Client(server_data["address"])
 
 
 def get_files(target_path):
@@ -195,41 +225,10 @@ def get_files(target_path):
         LOG.exception("uncaught exception")
     return samples
 
-
-def start_server_safe():
+def unpack_samples(samples, headless, ident, block):
     server_data = get_current_server_lock_data()
     if not server_data:
-        #Process(target=Server).start()
-        Thread(target=Server).start()
-        time.sleep(0.1)
-        # Server()
-    server_data = get_current_server_lock_data()
-    if server_data is None:
-        raise RuntimeError("Could not start roamerqueue server")
-    return server_data
-
-def connect_to_server(server_data):
-    return Client(server_data["address"])
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='RoAMer')
-    parser.add_argument('Samples', metavar='Sample', type=str, help='Path to sample or folder of samples')
-    parser.add_argument('--no-headless', action='store_false', help='Start the Sandbox in headless mode', dest="headless")
-    parser.add_argument('--vm', action='store', help='This can be used to force a VM past the config-file', default="")
-    parser.add_argument('--snapshot', action='store', help='This can be used to force a snapshot past the config-file', default="")
-    parser.add_argument('--config', action='store', help="Which config shall be used?", default="config")
-    parser.add_argument('--ident', action="store", help="Configure an identifier for the output.", default="")
-    parser.add_argument('--allow-start-server', action="store_true")
-    parser.add_argument('--block', action="store_true")
-
-    args = parser.parse_args()
-    
-    if args.allow_start_server:
-        server_data = start_server_safe()
-    else:
-        server_data = get_current_server_lock_data()
-        if not server_data:
-            raise ValueError("Server not available")
+        raise ValueError("Server not available")
     connection = connect_to_server(server_data)
 
     assert connection
@@ -237,13 +236,13 @@ if __name__ == "__main__":
     task_base = {
         "sample": None,
         "config": None,
-        "headless": args.headless,
+        "headless": headless,
         "vm": "",
         "snapshot": "",
         "id": None,
-        "ident": args.ident,
+        "ident": ident,
     }
-    samples = get_files(args.Samples)
+    samples = get_files(samples)
     ids = []
     for sample in samples:
         task = dict(**task_base)
@@ -253,7 +252,7 @@ if __name__ == "__main__":
         ids.append(id)
         connection.send(task)
 
-    if args.block and ids:
+    if block and ids:
         for message in iter(connection.recv, "STOP"):
             if message["id"] in ids:
                 if "logging" in message and message["logging"]:
@@ -268,4 +267,23 @@ if __name__ == "__main__":
     time.sleep(0.1)        
 
 
+##### CLI #####
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='RoAMer')
+    subparsers = parser.add_subparsers(dest="action", required=True)
+    send_parser = subparsers.add_parser("unpack")
+    send_parser.add_argument('Samples', metavar='Sample', type=str, help='Path to sample or folder of samples')
+    send_parser.add_argument('--no-headless', action='store_false', help='Start the Sandbox in headless mode', dest="headless")
+    send_parser.add_argument('--ident', action="store", help="Configure an identifier for the output.", default="")
+    send_parser.add_argument('--block', action="store_true")
+    server_parser = subparsers.add_parser("server")
+
+    args = parser.parse_args()
+
+    print(args.action)
+    if args.action == "server":
+        start_server_safe()
+    elif args.action == "unpack":
+        unpack_samples(args.Samples, args.headless, args.ident, args.block)
 
